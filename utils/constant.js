120af57ecf384c16897575b44dd571fb3a139ae6
maimingliang <maimingliang8@163.com>
2016-12-31 00:04:10 +0800

    utils commit

---
+++ utils/constant.js
@@ -0,0 +1,13 @@
+
+var BASE_URL = "http://gank.io/api/";
+var MEIZHI_URL = BASE_URL.concat("data/福利/50/");
+
+var DATA_IS_NULL = "获取数据为空，请重试";
+
+var PAGE_MAIN = "/pages/main/main";
+
+module.exports = {
+    BASE_URL: BASE_URL,
+    MEIZHI_URL: MEIZHI_URL,
+    DATA_IS_NULL: DATA_IS_NULL,
+}
